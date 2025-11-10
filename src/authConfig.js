b0da08de9046401c8f45bffd66fbbc95a7556c07
Wei Yung Choo <choo.choo@avanade.com>
2025-11-10 09:24:31 +0800

    Update redirectUri

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,8 @@ export const msalConfig = {
   auth: {
     clientId: "72a61b07-abb8-449c-a48f-7ceffadf7784", // 替换成 Azure AD 应用的 Client ID
     authority: "https://login.microsoftonline.com/9e72bcc5-65c2-436e-9207-796ec4ceaac3", // 替换成 Tenant ID
-    redirectUri: "http://localhost:3000"
+    //redirectUri: "http://localhost:3000"
+    redirectUri: "https://yellow-mushroom-0ee5aa610.3.azurestaticapps.net"
   }
 };
 
